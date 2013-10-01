2b0ca347cbc900075b9f98e10f8c55c56c401b4d
Christiano Milfont <cmilfont@gmail.com>
2013-09-30 22:28:41 -0300

    FIx no teste e troca da logo

--- spec/controllers/techniques_controller_spec.rb
+++ spec/controllers/techniques_controller_spec.rb
@@ -6,7 +6,7 @@ describe TechniquesController do
   describe "GET #index" do
         
     it "Should list Techniques" do
-      Technique.stub_chain(:all, :order).and_return [Technique.new]
+      Technique.stub_chain(:all, :order, :paginate).and_return [Technique.new]
       Technique.should_receive(:all).once
       get :index
     end
