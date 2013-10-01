84372411c324afe50d1e956171c5b787e619e3f4
Christiano Milfont <cmilfont@gmail.com>
2013-10-01 07:39:03 -0300

    vamos tentar com key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 #Martialarts::Application.config.session_store :cookie_store, key: '_martialarts_session'
 
-Martialarts::Application.config.session_store :iron_cache #, namespace: '_martialarts_session'
+Martialarts::Application.config.session_store :iron_cache, key: '_martialarts_session' #, namespace: '_martialarts_session'
