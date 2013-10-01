28335ecb540515ae9eb8e0b2713a942b52005679
Christiano Milfont <cmilfont@gmail.com>
2013-10-01 07:24:53 -0300

    configurando a session_Store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Martialarts::Application.config.session_store :cookie_store, key: '_martialarts_session'
+#Martialarts::Application.config.session_store :cookie_store, key: '_martialarts_session'
+
+AppName::Application.config.session_store :iron_cache, namespace: '_martialarts_session'
